380d52abd7941f11bc53b7dd10bfbd44cf33a709
TSCNEO <tscneo@gmail.com>
2016-09-29 12:04:03 +0200

    Added Script
    in Test

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd /hdd/
+wget https://github.com/TSCNEO/Picons_220x132_LightOnTransparent/archive/master.zip
+unzip master.zip
+mv Picons_220x132_LightOnTransparent-master/ picon/
+rm master.zip
