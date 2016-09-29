b4d57fce2d3f06dfe7d3e2c7f998da9f1ed441ee
TSCNEO <tscneo@gmail.com>
2016-09-29 12:20:24 +0200

    Fix Script

--- download.sh
+++ download.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cd /hdd/
+rm -R picon.old/
+mv picon/ picon.old/
 wget https://github.com/TSCNEO/Picons_220x132_LightOnTransparent/archive/master.zip
 unzip master.zip
 mv Picons_220x132_LightOnTransparent-master/ picon/
 rm master.zip
+chmod 777 picon/download.sh
